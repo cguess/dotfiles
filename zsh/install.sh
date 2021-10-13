0103b6648f7658b78bfc6fd6295be1bd5ac90fcd
Christopher Guess <cguess@gmail.com>
2021-10-13 12:27:04 -0400

    Update for new plugins git push

--- zsh/install.sh
+++ zsh/install.sh
@@ -5,6 +5,10 @@ echo "Setting proper permissions for oh-my-zsh"
 chmod 755 /usr/local/share/zsh
 chmod 755 /usr/local/share/zsh/site-functions
 
+echo "Installing autocomplete..."
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo "Add `zsh-autosuggestsion` to the `plugins` line in ~/.zshrc"
+
 echo "Installing powerline fonts..."
 git clone https://github.com/powerline/fonts.git --depth=1
 cd powerlinefonts
@@ -14,3 +18,5 @@ rm -rf powerlinefonts
 
 echo "Turning on zsh as default shell..."
 chsh -s /bin/zsh
+
+echo "Consider adding `dotenv` and `git` and `thfuck` to the `plugins` line in `~/.zshrc`"
